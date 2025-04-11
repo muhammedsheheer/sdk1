e855f9a3cc59cfea5abbc02fc3ac9c21d1b4fea2
sheheer <muhammedsheheer99@gmail.com>
2025-04-11 22:56:33 +0530

    higligh popup edited

--- src/app/(section)/Highlights.tsx
+++ src/app/(section)/Highlights.tsx
@@ -1,150 +1,3 @@
-// "use client";
-
-// import { useState, useEffect } from "react";
-// import Image from "next/image";
-// import { ArrowLeft, ArrowRight } from "lucide-react";
-// import React from "react";
-// import Link from "next/link";
-// import { Button } from "@/components/ui/button";
-
-// const images = [
-//   "/images/home/highlights/image1.png",
-//   "/images/home/highlights/image2.png",
-//   "/images/home/highlights/image3.png",
-//   "/images/home/highlights/image1.png",
-//   "/images/home/highlights/image2.png",
-//   "/images/home/highlights/image3.png",
-//   "/images/home/highlights/image2.png",
-//   "/images/home/highlights/image3.png",
-// ];
-
-// const Highlights: React.FC = () => {
-//   const [currentIndex, setCurrentIndex] = useState(0);
-//   const [imageWidth, setImageWidth] = useState(0);
-//   const imagesPerView = 3;
-
-//   useEffect(() => {
-//     const updateWidth = () => {
-//       const imgElement = document.querySelector(".slide-image");
-//       if (imgElement) {
-//         setImageWidth(imgElement.clientWidth + 16);
-//       }
-//     };
-
-//     updateWidth();
-//     window.addEventListener("resize", updateWidth);
-//     return () => window.removeEventListener("resize", updateWidth);
-//   }, []);
-
-//   const handleNext = () => {
-//     if (currentIndex < images.length - imagesPerView) {
-//       setCurrentIndex((prevIndex) => prevIndex + 1);
-//     }
-//   };
-
-//   const handlePrev = () => {
-//     if (currentIndex > 0) {
-//       setCurrentIndex((prevIndex) => prevIndex - 1);
-//     }
-//   };
-
-//   return (
-//     <section className="relative h-full w-full bg-[#fff] px-4 py-12 md:px-12 md:py-16 2xl:px-20">
-//       <div className="absolute right-[10%] top-0 hidden md:block">
-//         <Link href={"/table-booking"}>
-//           <Button
-//             className="relative z-40 mt-4 flex items-center justify-center gap-3 px-7 py-7 uppercase text-[#fff]"
-//             variant="imageB"
-//           >
-//             Book now{" "}
-//           </Button>
-//         </Link>
-//       </div>
-//       <div className="flex flex-col gap-6 md:gap-14">
-//         <div className="flex flex-col items-center justify-center gap-2 md:pb-8">
-//           <span className="font-birthstone text-2xl font-[400] tracking-[0.76px] text-[#8C7753]">
-//             Our story{" "}
-//           </span>
-//           <h6 className="text-center font-sofia_sans text-4xl font-[400] uppercase text-[#11331B] md:text-5xl">
-//             This Week at a Glance{" "}
-//           </h6>
-//           <div className="md:hidden">
-//             <Link href={"/menu"}>
-//               <Button
-//                 className="relative z-40 mt-4 flex items-center justify-center gap-3 px-10 py-7 uppercase"
-//                 variant="imageoutline"
-//               >
-//                 View Menu
-//               </Button>
-//             </Link>
-//           </div>
-//         </div>
-
-//         <div className="relative mx-auto w-full overflow-hidden">
-//           <div
-//             className="flex transition-transform duration-300 ease-in-out"
-//             style={{
-//               transform: `translateX(-${currentIndex * imageWidth}px)`,
-//             }}
-//           >
-//             {images.map((img, index) => (
-//               <div
-//                 key={index}
-//                 className="slide-image w-full flex-shrink-0 px-2 md:w-1/3"
-//               >
-//                 <Image
-//                   src={img}
-//                   alt={`Slide ${index + 1}`}
-//                   width={300}
-//                   height={200}
-//                   className="h-[300px] w-full object-cover md:h-[450px]"
-//                 />
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-
-//         <div className="relative mt-6 flex items-center justify-between px-10">
-//           <button
-//             onClick={handlePrev}
-//             className={`mt-3 rounded-full bg-[#11331B] p-3 text-[#fff] ring-1 ring-[#11331B] transition ${
-//               currentIndex === 0
-//                 ? "cursor-not-allowed opacity-50"
-//                 : "hover:bg-[#236236] hover:text-black"
-//             }`}
-//             disabled={currentIndex === 0}
-//           >
-//             <ArrowLeft className="h-6 w-6" />
-//           </button>
-//           <div className="flex justify-center gap-2">
-//             {images.map((_, index) => (
-//               <div
-//                 key={index}
-//                 className={`h-1 w-1 rotate-45 transition-all ${
-//                   currentIndex === index ? "bg-black" : "bg-[#236236]"
-//                 }`}
-//               />
-//             ))}
-//           </div>
-//           <button
-//             onClick={handleNext}
-//             className={`mt-3 rounded-full bg-[#11331B] p-3 text-[#fff] ring-1 ring-[#11331B] transition ${
-//               currentIndex >= images.length - imagesPerView
-//                 ? "cursor-not-allowed opacity-50"
-//                 : "hover:bg-[#236236] hover:text-black"
-//             }`}
-//             disabled={currentIndex >= images.length - imagesPerView}
-//           >
-//             <ArrowRight className="h-6 w-6" />
-//           </button>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Highlights;
-
 "use client";
 import { useEffect } from "react";
 import gsap from "gsap";
@@ -185,23 +38,23 @@ const images = [
     dj: "Friday live DJ 8 -2am",
     name: "Friday live DJ 8 -2am",
   },
-  {
-    image: "/images/home/highlights/image2.png",
-    food: "T&C only one offer at the time can be used.",
-    dj: "Saturday Live Dj 8 - 2am and Fire show girl dance ",
-    name: "Saturday Live Dj 8 - 2am and Fire show girl dance",
-  },
-  {
-    image: "/images/home/highlights/image3.png",
-    food: "T&C only one offer at the time can be used.",
-    dj: "live Dj 7-12 am",
-    name: "live Dj 7-12 am",
-  },
-  {
-    image: "/images/home/highlights/image3.png",
-    food: "T&C only one offer at the time can be used.",
-    name: "Electric Shisha at £25",
-  },
+  // {
+  //   image: "/images/home/highlights/image2.png",
+  //   food: "T&C only one offer at the time can be used.",
+  //   dj: "Saturday Live Dj 8 - 2am and Fire show girl dance ",
+  //   name: "Saturday Live Dj 8 - 2am and Fire show girl dance",
+  // },
+  // {
+  //   image: "/images/home/highlights/image3.png",
+  //   food: "T&C only one offer at the time can be used.",
+  //   dj: "live Dj 7-12 am",
+  //   name: "live Dj 7-12 am",
+  // },
+  // {
+  //   image: "/images/home/highlights/image3.png",
+  //   food: "T&C only one offer at the time can be used.",
+  //   name: "Electric Shisha at £25",
+  // },
 ];
 
 const Latest = ({}) => {
@@ -366,7 +219,7 @@ const Latest = ({}) => {
                       alt="right"
                       width={417}
                       height={531}
-                      className="about-arrow w-full md:h-[450px] md:w-[330px]"
+                      className="about-arrow w-full hover:cursor-pointer md:h-[450px] md:w-[330px]"
                     />
                   </SpecialPopup>
                 </CarouselItem>
@@ -392,34 +245,17 @@ const Latest = ({}) => {
             </div>
           </Carousel>
           <div className="grid grid-cols-2 gap-2 md:hidden">
-            <Image
-              src={"/images/home/highlights/image1.png"}
-              width={281}
-              height={74}
-              alt="image"
-              className="h-auto w-full"
-            />
-            <Image
-              src={"/images/home/highlights/image2.png"}
-              width={281}
-              height={74}
-              alt="image"
-              className="h-auto w-full"
-            />
-            <Image
-              src={"/images/home/highlights/image3.png"}
-              width={281}
-              height={74}
-              alt="image"
-              className="h-auto w-full"
-            />
-            <Image
-              src={"/images/home/highlights/image1.png"}
-              width={281}
-              height={74}
-              alt="image"
-              className="h-auto w-full"
-            />
+            {images.map((item, index) => (
+              <SpecialPopup key={index} item={item}>
+                <Image
+                  src={item.image}
+                  width={281}
+                  height={74}
+                  alt="image"
+                  className="h-auto w-full hover:cursor-pointer"
+                />
+              </SpecialPopup>
+            ))}
           </div>
         </div>
       </div>
